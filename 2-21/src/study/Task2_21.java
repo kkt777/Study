e35cda145a8649cbb06acd6b7b10379870e6dd50
kkt777 <hirachan051@gmail.com>
2026-04-09 22:45:15 +0900

    2-21

--- 2-21/src/study/Task2_21.java
+++ 2-21/src/study/Task2_21.java
@@ -1,63 +1,56 @@
 package study;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
-* Task2-21 : 課題内容
-* Java2章 中間チェックテスト理解/定着課題
-*
-* 問①〜問③の空欄を埋めて、指示通りに出力してください。
-*/
+ * 本課題では、代表的なコレクションフレームワークのArrayList及びHashMapを使い
+ * くだものとその値段を管理するコードを記述します。
+ * 
+ * 問①～問④まであります。
+ * 
+ */
 public class Task2_21 {
+    
+    public static final String orange = "オレンジ";
+    public static final String grape = "ぶどう";
+    public static final String apple = "りんご";
+    public static final String pear = "梨";
+
     public static void main(String[] args) {
-        /*
-         * 問①
-         * int型の変数 x と y を宣言し、それぞれ12と5を代入して、
-         * 次の演算結果を表示してください：
-         * ・x と y の和
-         * ・x と y の差
-         * ・x と y の積
-         * ・x と y の商
-         * ・x を y で割った余り
-         */
-        
-        int x = 12;
-        int y = 5;
+        // ① 変数名fruitsArrayListのデータ型StringでArrayListを宣言し、
+        // 変数orange、grape、apple、pearの値を順に格納して下さい。
+    	ArrayList<String> fruituArrayList = new ArrayList<>();
+    	
+    	fruituArrayList.add(apple);
+    	fruituArrayList.add(grape);
+    	fruituArrayList.add(pear);
+    	fruituArrayList.add(orange);
+    	
 
-        System.out.println("和：" + (x + y));
-        System.out.println("差：" + (x - y));
-        System.out.println("積：" + (x * y));
-        System.out.println("商：" + (x / y));
-        System.out.println("余り：" + (x % y));
+        // ② 変数名fruitsHashMapのHashMapを宣言（データ型　キーはString、値はInteger）し、
+        // キーにはfruitsArrayListに格納した値を格納し、
+        // 値にはオレンジは150円、ぶどうは120円、りんごは180円、梨は140円を格納して下さい。
+        // 値は数値のみで単位の円は入れないでください。
+    	HashMap<String,Integer> fruituHashMap = new HashMap<String, Integer>();
+    	
+    	fruituHashMap.put(orange, 150);
+    	fruituHashMap.put(grape, 120);
+    	fruituHashMap.put(apple, 180);
+    	fruituHashMap.put(pear, 140);
 
-        /*
-         * 問②
-         * int型の配列を宣言し、1から18までの偶数を格納して、
-         * ループを使用して順番に表示してください。
-         */
 
-        int[] arr = {2,4,6,8,10,12,14,16,18};
+        // ③ オレンジの値段を200円に上書きしてください。
+    	fruituHashMap.put(orange,200);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-        /*
-         * 問③
-         * int型の配列「numbers」内の奇数の合計を、
-         * メソッド「sumOddNumbers」を使って表示してください。
-         * ただし、0は偶数として扱います。
-         */
-        int[] numbers = {2, 4, 5, 7, 9, 10, 12, 13, 15};
-        int add = sumOddNumbers(numbers);
-        System.out.println("奇数の合計： " + add);
+
+        // ④ レッスン内容を参考に拡張for文を使い、
+        // 実行結果がターミナルと同じように表示されるよう出力して下さい。
+    	for(String string : fruituArrayList) {
+    		System.out.println(string + "の値段は" + fruituHashMap.get(string) + "です。");
+    	}
+
+
     }
 
-    public static int sumOddNumbers(int[] numbers) {
-        int add = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            // 問③ 奇数かどうかの判定を行い、奇数のみ加算してください。
-            if (numbers[i] % 2 !=0) {
-                add += numbers[i];
-            }
-        }
-        return add;
-    }
 }
